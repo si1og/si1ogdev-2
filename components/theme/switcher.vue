1cd68416e971fcff53fddec6c759d7600a5781e5
Ilya Semenov <sialogacc@gmail.com>
2025-04-25 16:22:43 +0300

    Add soped styles to theme-switcher

--- components/theme/switcher.vue
+++ components/theme/switcher.vue
@@ -53,3 +53,89 @@ onMounted(() => {
     </button>
   </div>
 </template>
+
+<style scoped>
+.theme-switch--conteiner {
+	position: relative;
+}
+.theme-switch {
+  border: none;
+  border-radius: 50%;
+  padding: 0.3rem;
+  cursor: pointer;
+  background: none;
+  transition: background 0.2s ease;
+  svg {
+    fill: var(--text-color-1);
+  }
+}
+
+.theme-switch:hover,
+.theme-switch--conteiner:has(button:focus) .theme-switch,
+.theme-switch--conteiner:has(.theme-switch__popover:hover) .theme-switch {
+  background: var(--bg-color-12);
+}
+
+.theme-switch__popover {
+	position: absolute;
+	right: 0;
+	top: 30px;
+  display: flex;
+  flex-direction: column;
+  min-width: 120px;
+  padding: 0.5rem 0;
+  border: none;
+  border-radius: 1rem;
+  background: var(--bg-color-1);
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.1);
+  transition: opacity 0.2s ease, transform 0.2s ease;
+  opacity: 0;
+  transform: scale(0.95);
+  transform-origin: top center;
+  pointer-events: none;
+	z-index: 1;
+}
+
+.theme-switch__popover::before {
+  position: absolute;
+  left: 50px;
+  top: -30px;
+  content: "";
+  width: 50px;
+  height: 30px;
+}
+
+.theme-switch:hover ~ .theme-switch__popover,
+.theme-switch:focus ~ .theme-switch__popover,
+.theme-switch__popover:hover,
+.theme-switch__popover:has(button:focus) {
+  opacity: 1;
+  transform: scale(1);
+  pointer-events: auto;
+}
+.theme-switch__select-button {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  background: none;
+  border: none;
+  width: 100%;
+  padding: 0.2rem 1rem;
+  font-size: 1rem;
+  cursor: pointer;
+  border-radius: 0.5rem;
+  transition: background 0.2s ease;
+  svg {
+    fill: var(--text-color-1)
+  }
+}
+
+.theme-switch__select-button:hover {
+  background: var(--bg-color-12);
+}
+
+.theme-switch__select-button.active {
+  background: var(--bg-color-12);
+  font-weight: 500;
+}
+</style>
